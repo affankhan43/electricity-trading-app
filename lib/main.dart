928274b09944b465fec46e7582c308603b11ccf3
unknown <affankhan627@gmail.com>
2019-11-01 17:17:22 +0500

    jwt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
-import 'splashScreen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'pages/login_page.dart';
+import 'pages/stats_page.dart';
+import 'pages/splashScreen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: "Trade Energy",
       routes: <String,WidgetBuilder>{
         "/LoginScreen": (BuildContext context) => LoginPage(),
+        "/StatsPage"  : (BuildContext context) => MainPage(),
       },
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
@@ -22,49 +24,4 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-
-}
-
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-
-  SharedPreferences sharedPreferences;
-
-  @override
-  void initState() {
-    super.initState();
-    checkLoginStatus();
   }
-
-  checkLoginStatus() async {
-    sharedPreferences = await SharedPreferences.getInstance();
-    if(sharedPreferences.getString("token") == null) {
-      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => LoginPage()), (Route<dynamic> route) => false);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Code Land", style: TextStyle(color: Colors.white)),
-        actions: <Widget>[
-          FlatButton(
-            onPressed: () {
-              sharedPreferences.clear();
-              sharedPreferences.commit();
-              Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => LoginPage()), (Route<dynamic> route) => false);
-            },
-            child: Text("Log Out", style: TextStyle(color: Colors.white)),
-          ),
-        ],
-      ),
-      body: Center(child: Text("Main Page")),
-      drawer: Drawer(),
-    );
-  }
-}
