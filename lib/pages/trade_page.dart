5f4e61a68539e3d125352506302b914a80059490
unknown <affankhan627@gmail.com>
2019-11-07 01:29:39 +0500

    trade

---
+++ lib/pages/trade_page.dart
@@ -0,0 +1,215 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import '../models/userModel.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class TradePage extends StatefulWidget {
+  @override
+  _TradePageState createState() => _TradePageState();
+}
+ 
+class _TradePageState extends State<TradePage> {
+  UserModel name = new UserModel(0,"","");
+  Future<String> rate;
+  SharedPreferences sharedPreferences;
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+    new GlobalKey<RefreshIndicatorState>();
+  //String name = "";
+
+  @override
+  void initState() {
+    super.initState();
+    // WidgetsBinding.instance
+    //   .addPostFrameCallback((_) => _refreshIndicatorKey.currentState.show());
+    checkLoginStatus();
+    getName().then((val)=>setState((){
+      name.name = val.name;
+      name.email = val.email;
+      name.id = val.id;
+    }));
+    rate =  fetchRate();
+  }
+  Future<UserModel> getName() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    var data = new UserModel(sharedPreferences.getInt("id"), sharedPreferences.getString("name"), sharedPreferences.getString("email"));
+    return data;
+  }
+  
+  checkLoginStatus() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    if(sharedPreferences.getString("token") == null) {
+      Navigator.of(context).pushReplacementNamed('/LoginScreen');
+    }
+  }
+
+  Future<String> fetchRate() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    String auth = "Bearer"+ sharedPreferences.getString("token");
+    final response =
+      await http.post(
+        'http://ec2-3-15-209-246.us-east-2.compute.amazonaws.com/ssuet-electric/public/api/getRate',
+        headers: {"authorization":auth,"Content-Type": "application/json","Accept": "application/json"}
+      );
+      
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      var data = json.decode(response.body);
+      rate = data.rate;
+    } else if(response.statusCode == 401){
+      sharedPreferences.clear();
+      sharedPreferences.commit();
+      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => LoginPage()), (Route<dynamic> route) => false);
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+}
+  Future<void> _refresh() async{
+    getName().then((val)=>setState((){
+      name.name = val.name;
+      name.email = val.email;
+      name.id = val.id;
+    }));
+    print('refreshing stocks...');
+    rate =  fetchRate();
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[600],
+        title: Text("Trade Settings", style: TextStyle(color: Colors.white)),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {
+              sharedPreferences.clear();
+              sharedPreferences.commit();
+              Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => LoginPage()), (Route<dynamic> route) => false);
+            },
+            child: Text("Log Out", style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
+      body: Container(
+        child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.green[600],Colors.white24],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter
+          ),
+        ),
+        child: new RefreshIndicator(
+          key: _refreshIndicatorKey,
+          onRefresh: _refresh,
+          child: ListView(
+            padding: EdgeInsets.only(top:20.0),
+            children:<Widget>[
+              Card(
+                color: Colors.transparent,
+                elevation: 0.0,
+                child: InkWell(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 15.0, bottom: 15.0, right: 15.0),
+                    decoration: BoxDecoration(
+                        color: Colors.green[300],
+                        borderRadius: BorderRadius.circular(25.0)
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                            Icon(Icons.settings, color: Colors.black38,size: 23.0,),
+                            Text(" Trade Settings",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 23.0,color: Colors.black38),),
+                        ],
+                    ),
+                  ),
+                ),
+              ),
+              FutureBuilder<String>(
+                future: rate,
+                builder: (context, snapshot){
+                  if (snapshot.hasData) {
+                    return Container(child:Column(children:<Widget>[
+                      statsList("Current Rate",snapshot.data),
+                    ]));
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return Center(child:CircularProgressIndicator(),);
+            },
+            ),
+            ],
+          ),
+          ),
+        ),
+      ),
+      drawer: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.green[600]
+        ),
+        child: Drawer(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top:30.0,bottom:30.0),
+                alignment: Alignment(0.0,0.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(name.name,style: TextStyle(fontWeight: FontWeight.w900,fontSize: 20.0)),
+                    Text(name.email,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 14.0)),
+                  ],
+                ),
+              ),
+              Divider(
+                color:Colors.black38,
+                thickness: 7.0,
+                indent: 7.0,
+                endIndent: 7.0,
+              ),
+              ListTile(
+                title: Text("Wallet"),
+                onTap: (){
+                  Navigator.of(context).pushNamed('/WalletPage');
+                },
+              ),
+              ListTile(
+                title: Text("Trading"),
+                onTap: (){
+                  Navigator.of(context).pushNamed('/TradePage');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  statsList(String title,String value)=>
+    Card(
+      color: Colors.transparent,
+      elevation: 0.0,
+      child: InkWell(
+        child: Container(
+          padding: EdgeInsets.only(top: 15.0, bottom: 15.0, right: 15.0),
+          decoration: BoxDecoration(
+              color: Colors.green[200],
+              borderRadius: BorderRadius.circular(25.0)
+          ),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                  Text(title+":",style: TextStyle(),),
+                  Text(value)
+              ],
+          ),
+        ),
+      ),
+    );
+}
